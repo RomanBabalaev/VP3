5e329b219557e2d2f41e13c6d6ce5b3864a2c182
Roman <87jormen@gmail.com>
2018-04-15 23:37:56 -0400

    Добавлена таксономия и каледарь

---
+++ public/wp-content/themes/VP3/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <section class="error-404 not-found">
+            <header class="page-header">
+                <h1 class="page-title"><?php _e( 'Houston, we have a problem! That page cant be found.', '' ); ?></h1>
+            </header>
+        </section>
+
+    </main>
+</div>
+
+<?php get_footer(); ?>
+
